21bb2b19432ec7e90d565f7177bcb25141250db2
umeckel <umeckel@gmail.com>
2025-10-12 13:27:02 +0200

    Fix ToDoTask: Using primary constructor

--- app/src/main/java/de/ulme/todo/data/models/ToDoTask.kt
+++ app/src/main/java/de/ulme/todo/data/models/ToDoTask.kt
@@ -5,10 +5,10 @@ import androidx.room.PrimaryKey
 import de.ulme.todo.util.Constants.TABLE_TASK
 
 @Entity(tableName = TABLE_TASK)
-data class ToDoTask {
+data class ToDoTask(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val title: String,
     val description: String,
     val priority: Priority,
-}
+)
