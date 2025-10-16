c38c9da8fbdff79990e5b2d7397cf748a7120c96
umeckel <umeckel@gmail.com>
2025-10-16 20:34:52 +0200

    Eliminate the line under the textfield: the indicatorline

--- app/src/main/java/de/ulme/todo/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/de/ulme/todo/ui/screens/list/ListAppBar.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -32,11 +31,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import de.ulme.todo.R
 import de.ulme.todo.components.PriorityItem
 import de.ulme.todo.data.models.Priority
@@ -121,6 +120,8 @@ fun SearchAppBar(
                 unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
                 focusedTextColor = MaterialTheme.colorScheme.onPrimaryContainer,
                 unfocusedTextColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
             ),
             value = text,
             onValueChange = { onTextChange(it) },
