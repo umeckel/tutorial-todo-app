8c491a7b5bade49a963a30d407ecc067b8f66aad
umeckel <umeckel@gmail.com>
2025-10-25 15:35:03 +0200

    Chapter 39: Added a SnackBar to give feedback for successfully performed an action

--- app/src/main/java/de/ulme/todo/ui/screens/list/ListScreen.kt
+++ app/src/main/java/de/ulme/todo/ui/screens/list/ListScreen.kt
@@ -7,10 +7,14 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,6 +22,7 @@ import de.ulme.todo.R
 import de.ulme.todo.ui.viewmodel.SharedViewModel
 import de.ulme.todo.util.Action
 import de.ulme.todo.util.SearchAppBarState
+import kotlinx.coroutines.launch
 
 @Composable
 fun ListScreen(
@@ -32,8 +37,16 @@ fun ListScreen(
     val searchAppBarState: SearchAppBarState by sharedViewModel.searchAppBarState
     val searchTextState: String by sharedViewModel.searchTextState
 
-    sharedViewModel.handleDatabaseAction(action)
-    Scaffold(topBar = {
+    val snackBarHostState = remember { SnackbarHostState() }
+    DisplaySnackBar(
+        snackBarHostState = snackBarHostState,
+        handleDatabaseActions = { sharedViewModel.handleDatabaseAction(action) },
+        taskTitle = sharedViewModel.title,
+        action = action,
+    )
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
+        topBar = {
         ListAppBar(
             sharedViewModel,
             searchAppBarState = searchAppBarState,
@@ -68,6 +81,29 @@ fun ListFab(
             ),
             tint = MaterialTheme.colorScheme.onTertiary,
         )
+    }
+}
+
+
+@Composable
+fun DisplaySnackBar(
+    snackBarHostState: SnackbarHostState,
+    handleDatabaseActions: () -> Unit,
+    taskTitle: String,
+    action: Action,
+) {
+    handleDatabaseActions()
+
+    val scope = rememberCoroutineScope()
+    LaunchedEffect(key1 = action) {
+        if (action != Action.NO_ACTION) {
+            scope.launch {
+                val snackBarResult = snackBarHostState.showSnackbar(
+                    message = "${action.name}: $taskTitle",
+                    actionLabel = "Ok",
+                )
+            }
+        }
     }
 }
 
